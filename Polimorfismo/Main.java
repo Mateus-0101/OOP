79c319bbf36b1258ccdd8692667dcbc8a3d184c4
Mateus-0101 <mateusantunes2342@gmail.com>
2023-12-06 14:06:01 +0000

    modified:   Polimorfismo/Main.java

--- Polimorfismo/Main.java
+++ Polimorfismo/Main.java
@@ -10,5 +10,21 @@ public class Main {
         inclusao.incluirTipo(new Conta());
         inclusao.incluirTipo(new ContaCorrente());
         inclusao.incluirTipo(new ContaPoupanca());
+
+        Sobrecarga overload = new Sobrecarga();
+        overload.media(2, 5);
+        overload.media(37.1, 54.3);
+        overload.media(34.2f, 63.7f);
+        overload.media("5", "9");
+
+        //Substituição/Override
+        Conta conta = new Conta();
+        conta.tipo();
+
+        ContaCorrente corrente = new ContaCorrente();
+        corrente.tipo();
+
+        ContaPoupanca poupanca = new ContaPoupanca();
+        poupanca.tipo();
     }
 }
