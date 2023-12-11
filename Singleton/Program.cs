cd4b0c42ea0cf779446330de15070f127b9de849
Mateus-0101 <mateusantunes2342@gmail.com>
2023-12-11 14:01:35 +0000

    new file:   Singleton/Program.cs 	new file:   Singleton/Singleton.csproj 	new file:   Singleton/bin/Debug/net7.0/Singleton.deps.json 	new file:   Singleton/bin/Debug/net7.0/Singleton.runtimeconfig.json 	new file:   Singleton/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   Singleton/obj/Debug/net7.0/Singleton.AssemblyInfo.cs 	new file:   Singleton/obj/Debug/net7.0/Singleton.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Singleton/obj/Debug/net7.0/Singleton.GlobalUsings.g.cs 	new file:   Singleton/obj/Singleton.csproj.nuget.dgspec.json 	new file:   Singleton/obj/Singleton.csproj.nuget.g.props 	new file:   Singleton/obj/Singleton.csproj.nuget.g.targets 	new file:   Singleton/obj/project.assets.json

---
+++ Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Singleton{
+
+    private static Singleton instance = null;
+    public string valor {get; set;}
+
+    private Singleton(String valor){
+        this.valor = valor;
+    }
+
+    public static Singleton getInstance(String valor){
+        if(instance == null){
+            instance = new Singleton(valor);
+        }
+
+        return instance;
+    }
+}
+
+class Program{
+    static void Main(string[] args){
+        Singleton singleton1 =  Singleton.getInstance("1");
+        Singleton singleton2 =  Singleton.getInstance("2");
+
+        Console.WriteLine(singleton1.valor);
+        Console.WriteLine(singleton2.valor);
+    }
+}
+
